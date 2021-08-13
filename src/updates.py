f7fabd040c766d333ebadb2e000f559492604f85
ong <ong2323@gmail.com>
2021-08-14 02:47:41 +0800

    added files

---
+++ src/updates.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-`````````````````````````````````````````
+
+import copy
+import torch
+from torch import nn
+import time
+from torch.utils.data import DataLoader, Dataset
+import gc
+
+class DatasetSplit(Dataset):
+    """An abstract Dataset class wrapped around Pytorch Dataset class.
+    """
+
+    def __init__(self, dataset, idxs):
+        self.dataset = dataset
+        self.idxs = [int(i) for i in idxs]
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self, item):
+        image, label = self.dataset[self.idxs[item]]
+        return torch.tensor(image), torch.tensor(label)
+
+class ScaffoldUpdate(object):
+    def __init__(self, args, dataset, idxs, logger, local_epoch):
+        self.args = args
+        self.logger = logger
+        self.local_ep = local_epoch
+        self.trainloader, self.validloader, self.testloader = self.train_val_test(
+            dataset, list(idxs))
+        self.device = 'cuda' if args.gpu else 'cpu'
+        self.criterion = nn.CrossEntropyLoss().to(self.device)
+        
+       
+
+    def train_val_test(self, dataset, idxs):
+        """
+        Returns train, validation and test dataloaders for a given dataset
+        and user indexes.
+        """
+        # split indexes for train, validation, and test (80, 10, 10)
+
+        idxs_train = idxs[:int(0.8*len(idxs))]
+        idxs_val = idxs[int(0.8*len(idxs)):int(0.9*len(idxs))]
+        idxs_test = idxs[int(0.9*len(idxs)):]
+
+        trainloader = DataLoader(DatasetSplit(dataset, idxs_train),
+                                 batch_size=self.args.local_bs, shuffle=True)
+        if len(idxs_val) < 10:
+            validloader = DataLoader(DatasetSplit(dataset, idxs_val),
+                                     batch_size=1, shuffle=False)
+            testloader = DataLoader(DatasetSplit(dataset, idxs_test),
+                                    batch_size=1, shuffle=False)
+        else:
+            validloader = DataLoader(DatasetSplit(dataset, idxs_val),
+                                     batch_size=int(len(idxs_val)/10), shuffle=False)
+            testloader = DataLoader(DatasetSplit(dataset, idxs_test),
+                                    batch_size=int(len(idxs_test)/10), shuffle=False)
+
+        return trainloader, validloader, testloader
+
+    def update_weights(self, model, global_round, control_local, control_global):
+        # Set mode to train model
+        model.train()
+        epoch_loss = []
+
+        global_model = copy.deepcopy(model)
+        start_time = time.time()
+        
+        decay = self.args.decay
+        if decay != 0:
+            learn_rate = self.args.lr * pow(decay, global_round)
+        else:
+            learn_rate = self.args.lr
+
+        # Set optimizer for the local updates
+        if self.args.optimizer == 'sgd':
+            optimizer = torch.optim.SGD(model.parameters(), lr=(learn_rate),
+                                        momentum=0.5)
+        elif self.args.optimizer == 'adam':
+            optimizer = torch.optim.Adam(model.parameters(), lr=(learn_rate),
+                                         weight_decay=1e-4)
+        
+        control_global_w = control_global.state_dict()
+        control_local_w = control_local.state_dict()
+        
+        count = 0
+        for iter in range(self.local_ep):
+            batch_loss = []
+            for batch_idx, (images, labels) in enumerate(self.trainloader):
+                images, labels = images.to(self.device), labels.to(self.device)
+
+                model.zero_grad()
+                log_probs = model(images)
+                loss = self.criterion(log_probs, labels)
+                loss.backward()
+                optimizer.step()
+
+                batch_loss.append(loss.item())
+                
+                local_weights = model.state_dict()
+                for w in local_weights:
+                    local_weights[w] = local_weights[w] - self.args.lr*(control_global_w[w]-control_local_w[w])
+                
+                #update local model params
+                model.load_state_dict(local_weights)
+                
+                count += 1
+                
+                
+
+                if self.args.verbose and (batch_idx % 10 == 0):
+                    print('| Global Round : {} | Local Epoch : {} | [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        global_round, iter, batch_idx * len(images),
+                        len(self.trainloader.dataset),
+                        100. * batch_idx / len(self.trainloader), loss.item()))
+                self.logger.add_scalar('loss', loss.item())
+
+            epoch_loss.append(sum(batch_loss)/len(batch_loss))
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        new_control_local_w = control_local.state_dict()
+        control_delta = copy.deepcopy(control_local_w)
+        global_weights = global_model.state_dict()
+        model_weights = model.state_dict()
+        for w in model_weights:
+            new_control_local_w[w] = new_control_local_w[w] - control_global_w[w] + (global_weights[w] - model_weights[w]) / (count * self.args.lr)
+            control_delta[w] = new_control_local_w - model_weights[w]
+        model.load_state_dict(new_control_local_w)
+        
+        return model.state_dict(), sum(epoch_loss) / len(epoch_loss), control_delta, time.time()- start_time
+
+    def inference(self, model):
+        """ Returns the inference accuracy and loss.
+        """
+
+        model.eval()
+        loss, total, correct = 0.0, 0.0, 0.0
+
+        for batch_idx, (images, labels) in enumerate(self.testloader):
+            images, labels = images.to(self.device), labels.to(self.device)
+
+            # Inference
+            outputs = model(images)
+            batch_loss = self.criterion(outputs, labels)
+            loss += batch_loss.item()
+
+            # Prediction
+            _, pred_labels = torch.max(outputs, 1)
+            pred_labels = pred_labels.view(-1)
+            correct += torch.sum(torch.eq(pred_labels, labels)).item()
+            total += len(labels)
+
+        accuracy = correct/total
+        return accuracy, loss
+
+def test_results(args, model, test_dataset):
+    """ Returns the test accuracy and loss.
+    """
+
+    model.eval()
+    loss, total, correct = 0.0, 0.0, 0.0
+
+    device = 'cuda' if args.gpu else 'cpu'
+    criterion = nn.NLLLoss().to(device)
+    testloader = DataLoader(test_dataset, batch_size=128,
+                            shuffle=False)
+
+    for batch_idx, (images, labels) in enumerate(testloader):
+        gc.collect()
+        torch.cuda.empty_cache()
+        images, labels = images.to(device), labels.to(device)
+
+        # Inference
+        outputs = model(images)
+        batch_loss = criterion(outputs, labels)
+        loss += batch_loss.item()
+
+        # Prediction
+        _, pred_labels = torch.max(outputs, 1)
+        pred_labels = pred_labels.view(-1)
+        correct += torch.sum(torch.eq(pred_labels, labels)).item()
+        total += len(labels)
+
+    accuracy = correct/total
+    return accuracy, loss
---
+++ src/utils.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import copy
+import torch
+from torchvision import datasets, transforms
+import numpy as np
+
+def get_dataset(args):
+    """ Returns train and test datasets and a user group which is a dict where
+    the keys are the user index and the values are the corresponding data for
+    each of those users.
+    """
+
+    if args.dataset == 'cifar':
+        data_dir = '../data/cifar/'
+       
+        train_transform = transforms.Compose(
+            [transforms.ToTensor(),
+             #transforms.RandomCrop(size=24),
+             transforms.RandomApply(torch.nn.ModuleList([
+                 transforms.ColorJitter(),]),p=0.5),
+             transforms.RandomAutocontrast(),
+             transforms.RandomHorizontalFlip(),
+             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
+
+        test_transform = transforms.Compose(
+                [transforms.ToTensor(),
+                 #transforms.RandomCrop(size=24),
+                 transforms.Normalize((0.4914,0.4822,0.4465),(0.247,0.243,0.261))])
+
+        train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
+                                       transform=train_transform)
+
+        test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
+                                      transform=test_transform)
+        
+        # sample training data amongst users
+        if args.iid:
+            # Sample IID user data from Mnist
+            user_groups = cifar_iid(train_dataset, args.num_users)
+        else:
+            # Sample Non-IID user data from Mnist
+            if args.unequal:
+                # Chose uneuqal splits for every user
+                raise NotImplementedError()
+            else:
+                # Chose euqal splits for every user
+                user_groups = cifar_noniid(train_dataset, args.num_users)
+
+    return train_dataset, test_dataset, user_groups
+
+def average_weights(w):
+    """
+    Returns the average of the weights.
+    """
+    w_avg = copy.deepcopy(w[0])
+    for key in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[key] += w[i][key]
+        w_avg[key] = torch.div(w_avg[key], len(w))
+    return w_avg
+
+
+def exp_details(args):
+    print('\nExperimental details:')
+    print(f'    Model     : {args.model}')
+    print(f'    Optimizer : {args.optimizer}')
+    print(f'    Learning  : {args.lr}')
+    print(f'    Global Rounds   : {args.epochs}\n')
+
+    print('    Federated parameters:')
+    if args.iid:
+        print('    IID')
+    else:
+        print('    Non-IID')
+    print(f'    Fraction of users  : {args.frac}')
+    print(f'    Local Batch size   : {args.local_bs}')
+    print(f'    Local Epochs       : {args.local_ep}\n')
+    return
+
+def cifar_iid(dataset, num_users):
+    """
+    Sample I.I.D. client data from CIFAR10 dataset
+    :param dataset:
+    :param num_users:
+    :return: dict of image index
+    """
+    num_items = int(len(dataset)/num_users)
+    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    for i in range(num_users):
+        dict_users[i] = set(np.random.choice(all_idxs, num_items,
+                                             replace=False))
+        all_idxs = list(set(all_idxs) - dict_users[i])
+    return dict_users
+
+
+def cifar_noniid(dataset, num_users):
+    """
+    Sample non-I.I.D client data from CIFAR10 dataset
+    :param dataset:
+    :param num_users:
+    :return:
+    """
+    num_shards, num_imgs = 200, 250
+    idx_shard = [i for i in range(num_shards)]
+    dict_users = {i: np.array([]) for i in range(num_users)}
+    idxs = np.arange(num_shards*num_imgs)
+    # labels = dataset.train_labels.numpy()
+    labels = np.array(dataset.targets)
+
+    # sort labels
+    idxs_labels = np.vstack((idxs, labels))
+    #stack into two rows, sort the labels row
+    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+    idxs = idxs_labels[0, :]
+
+    # divide and assign
+    for i in range(num_users):
+        rand_set = set(np.random.choice(idx_shard, 2, replace=False))
+        idx_shard = list(set(idx_shard) - rand_set)
+        for rand in rand_set:
+            dict_users[i] = np.concatenate(
+                (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
+    return dict_users
+
